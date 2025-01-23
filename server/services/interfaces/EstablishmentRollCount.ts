34a0b02df8925abc1eaa947acfff8f580ce9f899
Ashley Gyngell <ashley.gyngell@digital.justice.gov.uk>
2025-01-23 17:04:15 +0000

    MAP-1878: adding date-fns & MOJ dependencies

--- server/services/interfaces/EstablishmentRollCount.ts
+++ server/services/interfaces/EstablishmentRollCount.ts
@@ -1,4 +1,4 @@
-import PrisonRollCount, { ResidentialLocation } from '../../../../../hmpps-digital-prison-services/server/data/interfaces/prisonRollCount'
+import PrisonRollCount, { ResidentialLocation } from '../../../server/data/interfaces/prisonRollCount'
 
 export default interface EstablishmentRollCount {
   todayStats: {
