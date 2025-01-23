b382cf57efa66c7ad6e46c61d0083420eccf8afb
Ashley Gyngell <ashley.gyngell@digital.justice.gov.uk>
2025-01-23 15:12:02 +0000

    MAP-1878: adding services and routes

--- server/services/index.ts
+++ server/services/index.ts
@@ -1,14 +1,37 @@
 import { dataAccess } from '../data'
 import AuditService from './auditService'
+import EstablishmentRollService from './establishmentRollService'
+import MovementsService from './movementsService'
+import LocationService from './locationsService'
 
 export const services = () => {
-  const { applicationInfo, hmppsAuditClient } = dataAccess()
+  const {
+    prisonApiClientBuilder,
+    prisonerSearchApiClientBuilder,
+    locationsInsidePrisonApiClientBuilder,
+    applicationInfo,
+    hmppsAuditClient,
+  } = dataAccess()
 
   const auditService = new AuditService(hmppsAuditClient)
+
+  const establishmentRollService = new EstablishmentRollService(
+    prisonApiClientBuilder,
+    locationsInsidePrisonApiClientBuilder,
+  )
+  const movementsService = new MovementsService(
+    prisonApiClientBuilder,
+    prisonerSearchApiClientBuilder,
+    locationsInsidePrisonApiClientBuilder,
+  )
+  const locationsService = new LocationService(prisonApiClientBuilder, locationsInsidePrisonApiClientBuilder)
 
   return {
     applicationInfo,
     auditService,
+    establishmentRollService,
+    movementsService,
+    locationsService,
   }
 }
 
