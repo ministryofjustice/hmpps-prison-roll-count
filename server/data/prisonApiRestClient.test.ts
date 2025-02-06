96c4cdde1941663c2b3074e6497f906d21b9dae8
Ashley Gyngell <ashley.gyngell@digital.justice.gov.uk>
2025-02-06 17:38:18 +0000

    MAP-1878: unit tests

---
+++ server/data/prisonApiRestClient.test.ts
@@ -0,0 +1,86 @@
+import nock from 'nock'
+import config from '../config'
+import PrisonApiClient from './prisonApiRestClient'
+import restClientBuilder from '.'
+import { ApplicationInfo } from '../applicationInfo'
+import { locationMock } from '../mocks/locationMock'
+
+jest.mock('../applicationInfo.ts', () => {
+  return {
+    __esModule: true,
+    default: jest.fn(() => {
+      return {
+        applicationName: 'test',
+        buildNumber: '1',
+        gitRef: 'long ref',
+        gitShortHash: 'short ref',
+      } as ApplicationInfo
+    }),
+  }
+})
+
+const token = { access_token: 'token-1', expires_in: 300 }
+
+describe('prisonApiClient', () => {
+  let fakePrisonApi: nock.Scope
+  let prisonApiClient: PrisonApiClient
+
+  beforeEach(() => {
+    fakePrisonApi = nock(config.apis.prisonApi.url)
+    prisonApiClient = restClientBuilder('Prison API', config.apis.prisonApi, PrisonApiClient)(token.access_token)
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+    nock.cleanAll()
+  })
+
+  const mockSuccessfulPrisonApiCall = <TReturnData>(url: string, returnData: TReturnData) => {
+    fakePrisonApi.get(url).matchHeader('authorization', `Bearer ${token.access_token}`).reply(200, returnData)
+  }
+
+  describe('getCaseLoads', () => {
+    const caseloadMock = [
+      {
+        caseLoadId: 'LEI',
+        description: 'Leeds (HMP)',
+        type: 'INST',
+        caseloadFunction: 'GENERAL',
+        currentlyActive: true,
+      },
+    ]
+    it('should return data from api', async () => {
+      mockSuccessfulPrisonApiCall('/api/users/me/caseLoads?allCaseloads=true', caseloadMock)
+
+      const output = await prisonApiClient.getUserCaseLoads()
+      expect(output).toEqual(caseloadMock)
+    })
+  })
+
+  describe('getLocations', () => {
+    it('should return data from api', async () => {
+      mockSuccessfulPrisonApiCall('/api/users/me/locations', locationMock)
+
+      const output = await prisonApiClient.getUserLocations()
+      expect(output).toEqual(locationMock)
+    })
+  })
+
+  describe('setActiveCaseLoad', () => {
+    it('Should return data from the API', async () => {
+      const caseLoadMock = {
+        caseloadFunction: '',
+        caseLoadId: '',
+        currentlyActive: false,
+        description: 'Moorland',
+        type: '',
+      }
+      fakePrisonApi
+        .put('/api/users/me/activeCaseLoad')
+        .matchHeader('authorization', `Bearer ${token.access_token}`)
+        .reply(200, { body: 'RESPONSE' })
+      const output = await prisonApiClient.setActiveCaseload(caseLoadMock)
+      expect(output).toEqual({ body: 'RESPONSE' })
+    })
+  })
+})
---
+++ server/services/userService.test.ts
@@ -0,0 +1,69 @@
+import UserService from './userService'
+import PrisonApiRestClient from '../data/prisonApiRestClient'
+import { CaseLoad } from '../data/interfaces/caseLoad'
+import { Location } from '../data/interfaces/location'
+
+jest.mock('../data/prisonApiRestClient')
+
+const token = 'some token'
+
+describe('User service', () => {
+  let prisonApiClient: jest.Mocked<PrisonApiRestClient>
+  let userService: UserService
+
+  beforeEach(() => {
+    prisonApiClient = new PrisonApiRestClient(null) as jest.Mocked<PrisonApiRestClient>
+    userService = new UserService(() => prisonApiClient)
+  })
+
+  describe('getUserCaseLoads', () => {
+    it('retrieves list of user case loads', async () => {
+      const caseLoads = [{ caseLoadId: 'MDI' }] as CaseLoad[]
+      prisonApiClient.getUserCaseLoads.mockResolvedValue(caseLoads)
+
+      const result = await userService.getUserCaseLoads(token)
+
+      expect(result).toEqual(caseLoads)
+    })
+
+    it('propagates error', async () => {
+      prisonApiClient.getUserCaseLoads.mockRejectedValue(new Error('some error'))
+
+      await expect(userService.getUserCaseLoads(token)).rejects.toEqual(new Error('some error'))
+    })
+  })
+
+  describe('getUserLocations', () => {
+    it('retrieves list of user locations', async () => {
+      const locations = [{ locationId: 12345 }] as Location[]
+      prisonApiClient.getUserLocations.mockResolvedValue(locations)
+
+      const result = await userService.getUserLocations(token)
+
+      expect(result).toEqual(locations)
+    })
+
+    it('propagates error', async () => {
+      prisonApiClient.getUserLocations.mockRejectedValue(new Error('some error'))
+
+      await expect(userService.getUserLocations(token)).rejects.toEqual(new Error('some error'))
+    })
+  })
+
+  describe('setActiveCaseload', () => {
+    it('makes call to set active case load', async () => {
+      const caseLoad = { caseLoadId: 'MDI' } as CaseLoad
+      await userService.setActiveCaseload(token, caseLoad)
+
+      expect(prisonApiClient.setActiveCaseload).toHaveBeenCalledWith(caseLoad)
+    })
+
+    it('propagates error', async () => {
+      prisonApiClient.setActiveCaseload.mockRejectedValue(new Error('some error'))
+
+      await expect(userService.setActiveCaseload(token, { caseLoadId: 'MDI' } as CaseLoad)).rejects.toEqual(
+        new Error('some error'),
+      )
+    })
+  })
+})
