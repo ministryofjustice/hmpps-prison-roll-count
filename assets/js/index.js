21547da0bc4419f854b598b9d8a4a1ed0ca65620
Ashley Gyngell <ashley.gyngell@digital.justice.gov.uk>
2025-02-07 18:04:15 +0000

    MAP-1878: attempt at fix for custom js files not loading

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,12 @@
 import * as govukFrontend from 'govuk-frontend'
 import * as mojFrontend from '@ministryofjustice/frontend'
+import establishmentRoll from './establishment-roll'
+import initMoj from './initMoj'
+import printPage from './printPage'
 
 govukFrontend.initAll()
 mojFrontend.initAll()
+establishmentRoll()
+printPage()
+initMoj()
 
