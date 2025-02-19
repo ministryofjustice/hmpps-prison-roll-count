7eb1cd00dddaaf325bbe92fff0518e8b93f9a2fc
Adam Sharp <me@thource.dev>
2025-02-19 10:56:18 +0000

    Add GA to connect-src (#26)

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -20,7 +20,13 @@ export default function setUpWebSecurity(): Router {
   // <link href="http://example.com/" rel="stylesheet" nonce="{{ cspNonce }}">
   // This ensures only scripts we trust are loaded, and not anything injected into the
   // page by an attacker.
-  const connectSrc = ["'self'", (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`]
+  const connectSrc = [
+    "'self'",
+    '*.google-analytics.com',
+    '*.analytics.google.com',
+    '*.googletagmanager.com',
+    (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`,
+  ]
   const scriptSrc = [
     "'self'",
     '*.google-analytics.com',
