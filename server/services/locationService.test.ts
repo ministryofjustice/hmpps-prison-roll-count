690884e2b361c18c8396946dccbaed6bfb537212
Ashley Gyngell <ashley.gyngell@digital.justice.gov.uk>
2025-03-03 18:50:28 +0000

    MAP-1734: improve location service test coverage.

--- server/services/locationService.test.ts
+++ server/services/locationService.test.ts
@@ -3,7 +3,7 @@ import LocationService from './locationsService'
 import { locationMock } from '../mocks/locationMock'
 import locationsInsidePrisonApiClientMock from '../test/mocks/locationsInsidePrisonApiClientMock'
 
-describe('establishmentRollService', () => {
+describe('LocationService', () => {
   let locationRollService: LocationService
 
   beforeEach(() => {
@@ -14,12 +14,75 @@ describe('establishmentRollService', () => {
   })
 
   describe('getLocationInfo', () => {
-    it('should call api and return response', async () => {
+    it('should call prison API and return response', async () => {
       prisonApiClientMock.getLocation = jest.fn().mockResolvedValueOnce(locationMock)
 
-      const locationInfo = await locationRollService.getLocationInfo('token', 'loc')
+      const locationInfo = await locationRollService.getLocationInfo('token', 'MDI')
 
+      expect(prisonApiClientMock.getLocation).toHaveBeenCalledWith('MDI')
       expect(locationInfo).toEqual(locationMock)
+    })
+
+    it('should handle errors when calling prisonApiClient fails', async () => {
+      prisonApiClientMock.getLocation = jest.fn().mockRejectedValueOnce(new Error('API Error'))
+
+      try {
+        await locationRollService.getLocationInfo('token', 'MDI')
+      } catch (error) {
+        expect(error).toEqual(new Error('API Error'))
+      }
+    })
+  })
+
+  describe('isActivePrison', () => {
+    it('should call locations API, returning true if prison is active', async () => {
+      locationsInsidePrisonApiClientMock.isActivePrison = jest.fn().mockResolvedValueOnce(true)
+
+      const isActive = await locationRollService.isActivePrison('token', 'prisonId')
+
+      expect(locationsInsidePrisonApiClientMock.isActivePrison).toHaveBeenCalledWith('prisonId')
+      expect(isActive).toBe(true)
+    })
+
+    it('should call locations API, returning false if prison is inactive', async () => {
+      locationsInsidePrisonApiClientMock.isActivePrison = jest.fn().mockResolvedValueOnce(false)
+
+      const isActive = await locationRollService.isActivePrison('token', 'prisonId')
+
+      expect(locationsInsidePrisonApiClientMock.isActivePrison).toHaveBeenCalledWith('prisonId')
+      expect(isActive).toBe(false)
+    })
+
+    it('should handle errors when call to locations API fails', async () => {
+      locationsInsidePrisonApiClientMock.isActivePrison = jest.fn().mockRejectedValueOnce(new Error('API Error'))
+
+      try {
+        await locationRollService.isActivePrison('token', 'prisonId')
+      } catch (error) {
+        expect(error).toEqual(new Error('API Error'))
+      }
+    })
+  })
+
+  describe('getInternalLocationInfo', () => {
+    it('should call locationsI API and return the internal location info', async () => {
+      const internalLocationMock = { id: '123', description: 'Location Description' }
+      locationsInsidePrisonApiClientMock.getLocation = jest.fn().mockResolvedValueOnce(internalLocationMock)
+
+      const internalLocationInfo = await locationRollService.getInternalLocationInfo('token', 'MDI')
+
+      expect(locationsInsidePrisonApiClientMock.getLocation).toHaveBeenCalledWith('MDI')
+      expect(internalLocationInfo).toEqual(internalLocationMock)
+    })
+
+    it('should handle errors when locations API call fails', async () => {
+      locationsInsidePrisonApiClientMock.getLocation = jest.fn().mockRejectedValueOnce(new Error('API Error'))
+
+      try {
+        await locationRollService.getInternalLocationInfo('token', 'MDI')
+      } catch (error) {
+        expect(error).toEqual(new Error('API Error'))
+      }
     })
   })
 })
